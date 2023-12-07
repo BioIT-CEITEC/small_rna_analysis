164dfeab519b3ee133888dcf8aedddb520eecef1
Alejandro Medaglia <amedagliamata2@gmail.com>
2023-12-07 03:42:02 +0000

    Add new file

---
+++ wrappers/rrna_mapping/script.py
@@ -0,0 +1,70 @@
+###########################################
+# wrapper for rule: miRNA_preprocessing
+###########################################
+import subprocess
+from os.path import dirname
+from snakemake.shell import shell
+
+shell.executable("/bin/bash")
+log_filename = str(snakemake.log)
+
+f = open(log_filename, "wt")
+f.write("\n##\n## RULE: rrna mapping \n##\n")
+f.close()
+
+version = str(subprocess.Popen("conda list", shell = True, stdout = subprocess.PIPE).communicate()[0], 'utf-8')
+f = open(log_filename, "at")
+f.write("\n##\n## CONDA: " + version + "\n")
+f.close()
+
+command = "mkdir -p " + dirname(snakemake.output.mapping_stats)
+f = open(log_filename, "at")
+f.write("\n##\n## COMMAND: " + command + "\n")
+f.close()
+shell(command)
+
+command = "STAR --runMode alignReads --runThreadN " + str(snakemake.params.nthreads) + " --genomeDir " + str(snakemake.params.genome_ref) + " --readFilesCommand zcat --readFilesIn " + str(snakemake.input.clean) + \
+    " --outFileNamePrefix " + str(snakemake.input.clean).replace("_clean_collapsed.fastq.gz", "") + ".rnar. --outFilterMultimapNmax " + str(snakemake.params.max_multimap) + " --outFilterMatchNmin " + str(snakemake.params.min_multimap) + " --outFilterMismatchNoverReadLmax " + \
+        str(snakemake.params.filter_mismatch) + " --outFilterMultimapScoreRange " + str(snakemake.params.score_range) + " --outFilterScoreMinOverLread " + str(snakemake.params.min_over) + " --outFilterMismatchNmax " + str(snakemake.params.filtermax_mismatch) + \
+            " --alignIntronMax " + str(snakemake.params.intron_max) + " --alignIntronMin " + str(snakemake.params.intron_min) + " --outSAMheaderHD @HD VN:1.4 SO:coordinate --outSAMunmapped Within --outReadsUnmapped Fastx --outFilterType Normal --outSAMattributes All --twopassMode None --seedSearchStartLmax " + \
+                str(snakemake.params.seed_search) + " --winAnchorMultimapNmax " + str(snakemake.params.anchor_multimap) + " --outMultimapperOrder Random --outSAMtype BAM Unsorted --alignEndsType EndToEnd" + " >> " + log_filename + " 2>&1"
+f = open(log_filename, "at")
+f.write("\n##\n## COMMAND: " + command + "\n")
+f.close()
+shell(command)
+
+command = "mv " + dirname(snakemake.input.clean) + "/*.rnar.* " + dirname(snakemake.output.bam)
+f = open(log_filename, "at")
+f.write("\n##\n## COMMAND: " + command + "\n")
+f.close()
+shell(command)
+
+command = "samtools view " + snakemake.output.bam + " | grep -w \"NH:i:1\" - | cut -f1 - | cut -d\"_\" -f3 - | sed 's/x//g' | awk '{{ sum += $1 }} END {{ print sum }}' >> " + snakemake.output.mapping_stats
+f = open(log_filename, "at")
+f.write("\n##\n## COMMAND: " + command + "\n")
+f.close()
+shell(command)
+
+command = "samtools view " + snakemake.output.bam + " | grep -v \"NH:i:1\" - | grep -v \"NH:i:0\" - | cut -f1 - | sort | uniq | cut -d\"_\" -f3 - | sed 's/x//g' | awk '{{ sum += $1 }} END {{ print sum }}' >> " + snakemake.output.mapping_stats
+f = open(log_filename, "at")
+f.write("\n##\n## COMMAND: " + command + "\n")
+f.close()
+shell(command)
+
+command = "mv " + str(snakemake.output.unmapped).replace(".fastq.gz", "") + " " + str(snakemake.output.unmapped).replace(".gz", "") + " >> " + log_filename + " 2>&1"
+f = open(log_filename, "at")
+f.write("\n##\n## COMMAND: " + command + "\n")
+f.close()
+shell(command)
+
+command = "sed -i 's/0:N://g' " + str(snakemake.output.unmapped).replace(".gz", "") + " >> " + log_filename + " 2>&1"
+f = open(log_filename, "at")
+f.write("\n##\n## COMMAND: " + command + "\n")
+f.close()
+shell(command)
+
+command = "gzip " + str(snakemake.output.unmapped).replace(".gz", "") + " >> " + log_filename + " 2>&1"
+f = open(log_filename, "at")
+f.write("\n##\n## COMMAND: " + command + "\n")
+f.close()
+shell(command)
