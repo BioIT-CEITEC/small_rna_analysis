d77edbfbd3b2148d15d064365cf27e45d9479ac4
Alejandro Medaglia <amedagliamata2@gmail.com>
2023-12-07 03:38:39 +0000

    Add new file

---
+++ wrappers/quality_control/script.py
@@ -0,0 +1,28 @@
+###########################################
+# wrapper for rule: miRNA_preprocessing
+###########################################
+
+import subprocess
+from os.path import dirname
+from os.path import basename
+from snakemake.shell import shell
+
+shell.executable("/bin/bash")
+log_filename = str(snakemake.log)
+
+f = open(log_filename, "wt")
+f.write("\n##\n## RULE: first quality control \n##\n")
+f.close()
+
+version = str(subprocess.Popen("conda list", shell = True, stdout = subprocess.PIPE).communicate()[0], 'utf-8')
+f = open(log_filename, "at")
+f.write("\n##\n## CONDA: " + version + "\n")
+f.close()
+
+command = "mkdir -p " + dirname(snakemake.output.clean) + " >> " + log_filename + " 2>&1"
+
+command = "fastqc --outdir " + dirname(snakemake.output.trim) + " --format " + str(snakemake.params.formats) + " --threads " + str(snakemake.threads) + " " + dirname(snakemake.input.trim) + "/*.fastq.gz >> " + log_filename + " 2>&1"
+f = open(log_filename, "at")
+f.write("\n##\n## COMMAND: " + command + "\n")
+f.close()
+shell(command)
