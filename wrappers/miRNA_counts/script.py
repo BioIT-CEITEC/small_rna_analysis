0fe311b130f65a03b0d33243edc471acc2b1c946
Alejandro Medaglia <amedagliamata2@gmail.com>
2023-12-07 03:32:59 +0000

    Update file script.py

--- wrappers/miRNA_counts/script.py
+++ wrappers/miRNA_counts/script.py
@@ -9,7 +9,7 @@ shell.executable("/bin/bash")
 log_filename = str(snakemake.log)
 
 f = open(log_filename, "wt")
-f.write("\n##\n## RULE: merge first qc \n##\n")
+f.write("\n##\n## RULE: mirna counts \n##\n")
 f.close()
 
 version = str(subprocess.Popen("conda list", shell = True, stdout = subprocess.PIPE).communicate()[0], 'utf-8')
