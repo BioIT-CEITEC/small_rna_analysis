6b2dfeb09cb219d9b1ebc147f87bd9adeaf4e78f
amedagliamata <amedagliamata2@gmail.com>
2024-02-20 15:49:37 +0100

    added BR utils for loading miRNA data

--- wrappers/rrna_mapping/script.py
+++ wrappers/rrna_mapping/script.py
@@ -23,7 +23,7 @@ f.write("\n##\n## COMMAND: " + command + "\n")
 f.close()
 shell(command)
 
-command = "STAR --runMode alignReads --runThreadN " + str(snakemake.params.nthreads) + " --genomeDir " + snakemake.input.rrna + \
+command = "STAR --runMode alignReads --runThreadN " + str(snakemake.params.nthreads) + " --genomeDir " + dirname(snakemake.input.rrna) + \
           " --readFilesCommand zcat" + \
           " --readFilesIn " + str(snakemake.input.clean) + \
           " --outFileNamePrefix " + dirname(snakemake.output.bam) + "/" + snakemake.wildcards.sample + ".rnar." + \
