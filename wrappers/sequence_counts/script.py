fcf6b03903a11a286a4d8d539c4737f48fc9e79c
Alejandro Medaglia <amedagliamata2@gmail.com>
2024-04-09 19:53:36 +0000

    corrected seq counts script to have the correct format

--- wrappers/sequence_counts/script.py
+++ wrappers/sequence_counts/script.py
@@ -24,25 +24,25 @@ f.write("\n##\n## COMMAND: " + command + "\n")
 f.close()
 shell(command)
 
-command = "mkdir -p " + dirname(snakemake.output.summary) + snakemake.wildcards.sample + "_zip_folder >> " + log_filename + " 2>&1"
+command = "mkdir -p " + dirname(snakemake.output.summary) + "/" + snakemake.wildcards.sample + "_zip_folder >> " + log_filename + " 2>&1"
 f = open(log_filename, "at")
 f.write("\n##\n## COMMAND: " + command + "\n")
 f.close()
 shell(command)
 
-command = "unzip -o " + dirname(snakemake.input.trimmed) + "/\"*fastqc.zip\"" + " -d " + dirname(snakemake.output.summary) + snakemake.wildcards.sample + "_zip_folder"
+command = "unzip -o " + dirname(snakemake.input.trimmed) + "/\"" + snakemake.wildcards.sample +  "*fastqc.zip\"" + " -d " + dirname(snakemake.output.summary) + "/" + snakemake.wildcards.sample + "_zip_folder"
 f = open(log_filename, "at")
 f.write("\n##\n## COMMAND: " + command + "\n")
 f.close()
 shell(command)
 
-command = "grep \"Total Sequences\" " + dirname(snakemake.output.summary) + snakemake.wildcards.sample + "_zip_folder/" + snakemake.wildcards.sample + ".*/*.txt >> " + snakemake.output.summary + " 2>> " + log_filename + " 2>&1"
+command = "grep \"Total Sequences\" " + dirname(snakemake.output.summary) + "/" + snakemake.wildcards.sample + "_zip_folder/" + snakemake.wildcards.sample + ".*/*.txt >> " + snakemake.output.summary + " 2>> " + log_filename + " 2>&1"
 f = open(log_filename, "at")
 f.write("\n##\n## COMMAND: " + command + "\n")
 f.close()
 shell(command)
 
-command = "rm -r " + dirname(snakemake.output.summary) + snakemake.wildcards.sample + "_zip_folder >> " + log_filename + " 2>&1"
+command = "rm -r " + dirname(snakemake.output.summary) + "/" + snakemake.wildcards.sample + "_zip_folder >> " + log_filename + " 2>&1"
 f = open(log_filename, "at")
 f.write("\n##\n## COMMAND: " + command + "\n")
 f.close()
